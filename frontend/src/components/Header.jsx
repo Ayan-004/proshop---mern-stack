c0cb58f027ac0ff20f5798c26e3ee882594e2c96
Ayan Shaikh <ayyanjsk@gmail.com>
2025-06-01 23:19:32 +0530

    Add Links

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -2,24 +2,25 @@ import React from "react";
 import { Navbar, Nav, Container } from "react-bootstrap";
 import { FaShoppingCart, FaUser } from "react-icons/fa";
 import logo from "../assets/logo.png";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
     <header>
       <Navbar bg="dark" variant="dark" expand="md" collapseOnSelect className="montserrat">
         <Container>
-          <Navbar.Brand href="/" className="montserrat-bold" >
+          <Navbar.Brand as={Link} to='/' className="montserrat-bold" >
           <img src={logo} alt='ProShop' />
           ProShop
           </Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nab">
             <Nav className="ms-auto">
-              <Nav.Link href="/cart">
+              <Nav.Link as={Link} to='/cart'>
                 <FaShoppingCart className="me-1 mb-1" />
                 Cart
               </Nav.Link>
-              <Nav.Link href="/Login">
+              <Nav.Link as={Link} to='/Login'>
                 <FaUser className="me-1 mb-1" />
                 Sign In
               </Nav.Link>
--- frontend/src/components/Product.jsx
+++ frontend/src/components/Product.jsx
@@ -1,21 +1,30 @@
 import { Card } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import Rating from "./Rating";
 
 const Product = ({ product }) => {
   return (
     <Card className="my-3 p-3 rounded-5 montserrat">
-      <a href={`/product/${product._id}`}>
-        <Card.Img src={product.image} variant="top" className="rounded-4"/>
-      </a>
+      <Link to={`/product/${product._id}`}>
+        <Card.Img src={product.image} variant="top" className="rounded-4" />
+      </Link>
 
       <Card.Body>
-        <a href={`/product/${product._id}`} className="no-underline">
-          <Card.Title as="div" style={{ color: '#000000' }}>
+        <Link to={`/product/${product._id}`} className="no-underline">
+          <Card.Title as="div" className="product-title text-black">
             <strong>{product.name}</strong>
           </Card.Title>
-        </a>
+        </Link>
+
+        <Card.Text as="div">
+          <Rating
+            value={product.rating}
+            text={`${product.numReviews} reviews`}
+          />
+        </Card.Text>
 
         <Card.Text as="h3" className="fs-5">
-            ₹{product.price.toLocaleString('en-IN')}
+          ₹{product.price.toLocaleString("en-IN")}
         </Card.Text>
       </Card.Body>
     </Card>
