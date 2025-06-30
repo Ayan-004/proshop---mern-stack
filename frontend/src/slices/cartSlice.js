519c9bf7866e3c8d18eb12eb79464d83623c6967
Ayan Shaikh <ayyanjsk@gmail.com>
2025-06-30 18:01:44 +0530

    add savePaymentMethod reducer

--- frontend/src/slices/cartSlice.js
+++ frontend/src/slices/cartSlice.js
@@ -3,7 +3,7 @@ import { updateCart } from "../utils/cartUtils";
 
 const initialState = localStorage.getItem("cart")
   ? JSON.parse(localStorage.getItem("cart"))
-  : { cartItems: [], shippingAddress: {}, payment: "PayPal" };
+  : { cartItems: [], shippingAddress: {}, paymentMethod: "PayPal" };
 
 const cartSlice = createSlice({
   name: "cart",
@@ -34,10 +34,14 @@ const cartSlice = createSlice({
       state.shippingAddress = action.payload;
       return updateCart(state);
     },
+    savePaymentMethod: (state, action) => {
+      state.paymentMethod = action.payload;
+      return updateCart(state);
+    }
   },
 });
 
-export const { addToCart, removeFromCart, saveShippingAddress } =
+export const { addToCart, removeFromCart, saveShippingAddress, savePaymentMethod } =
   cartSlice.actions;
 
 export default cartSlice.reducer;
