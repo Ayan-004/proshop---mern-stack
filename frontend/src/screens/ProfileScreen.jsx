a4db0c2d656aed5cc7da4dc17de6ce5ce8495b65
Ayan Shaikh <ayyanjsk@gmail.com>
2025-07-08 17:04:41 +0530

    remove unused import

--- frontend/src/screens/ProfileScreen.jsx
+++ frontend/src/screens/ProfileScreen.jsx
@@ -1,7 +1,6 @@
 import { useState, useEffect } from "react";
 import { Table, Form, Button, Row, Col, Nav } from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
-import { LinkContainer } from "react-router-bootstrap";
 import { toast } from "react-toastify";
 import { FaTimes } from "react-icons/fa"
 import Message from "../components/Message";
