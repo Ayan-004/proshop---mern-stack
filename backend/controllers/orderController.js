4a0757c0758b6bc2f60bb604427a43b83234bc7c
Ayan Shaikh <ayyanjsk@gmail.com>
2025-07-14 18:10:56 +0530

    Add pagination logic

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -113,8 +113,15 @@ const updateOrderToDelivered = asyncHandler(async (req, res) => {
 // @route  GET/api/orders
 // @access Private/Admin
 const getOrders = asyncHandler(async (req, res) => {
-  const orders = await Order.find({}).populate("user", "name");
-  res.status(200).json(orders);
+  const pageSize = 10;
+  const page = Number(req.query.pageNumber) || 1;
+  const count = await Order.countDocuments();
+
+  const orders = await Order.find({})
+    .populate("user", "name")
+    .limit(pageSize)
+    .skip(pageSize * (page - 1));
+  res.json({ orders, page, pages: Math.ceil(count / pageSize) });
 });
 
 export {
--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -5,8 +5,14 @@ import Product from "../models/productModel.js";
 // @route GET/api/products
 // @access Public
 const getProducts = asyncHandler(async (req, res) => {
-  const products = await Product.find({});
-  res.json(products);
+  const pageSize = 4;
+  const page = Number(req.query.pageNumber) || 1;
+  const count = await Product.countDocuments();
+
+  const products = await Product.find({})
+    .limit(pageSize)
+    .skip(pageSize * (page - 1));
+  res.json({ products, page, pages: Math.ceil(count / pageSize) });
 });
 
 // @desc   Fetch a products
@@ -47,26 +53,26 @@ const createProduct = asyncHandler(async (req, res) => {
 // @route PUT/api/products/:id
 // @access Private/Admin
 const updateProduct = asyncHandler(async (req, res) => {
-  const { name, price, description, image, brand, category, countInStock } = req.body;
+  const { name, price, description, image, brand, category, countInStock } =
+    req.body;
 
   const product = await Product.findById(req.params.id);
 
-  if(product) {
-    product.name = name,
-    product.price = price,
-    product.description = description,
-    product.image = image,
-    product.brand = brand,
-    product.category = category,
-    product.countInStock = countInStock
+  if (product) {
+    (product.name = name),
+      (product.price = price),
+      (product.description = description),
+      (product.image = image),
+      (product.brand = brand),
+      (product.category = category),
+      (product.countInStock = countInStock);
 
     const updatedProduct = await product.save();
-    res.json(updatedProduct)
+    res.json(updatedProduct);
   } else {
-    res.status(404)
-    throw new Error("Resource not found")
+    res.status(404);
+    throw new Error("Resource not found");
   }
-
 });
 
 // @desc   Delete product
@@ -75,14 +81,61 @@ const updateProduct = asyncHandler(async (req, res) => {
 const deleteProduct = asyncHandler(async (req, res) => {
   const product = await Product.findById(req.params.id);
 
-  if(product) {
-   await Product.deleteOne({ _id: product._id })
-   res.status(200).json({ message: 'Product deleted' })
+  if (product) {
+    await Product.deleteOne({ _id: product._id });
+    res.status(200).json({ message: "Product deleted" });
   } else {
-    res.status(404)
-    throw new Error("Resource not found")
+    res.status(404);
+    throw new Error("Resource not found");
   }
-
 });
 
-export { getProducts, getProductById, createProduct, updateProduct, deleteProduct };
+// @desc   Create product reviews
+// @route  POST /api/products/:id/reviews
+// @access Private
+const createProductReview = asyncHandler(async (req, res) => {
+  const { rating, comment } = req.body;
+
+  const product = await Product.findById(req.params.id);
+
+  if (product) {
+    const alreadyReviewed = product.reviews.find(
+      (review) => review.user.toString() === req.user._id.toString()
+    );
+
+    if (alreadyReviewed) {
+      res.status(400);
+      throw new Error("Product already reviewed");
+    }
+
+    const review = {
+      name: req.user.name,
+      rating: Number(rating),
+      comment,
+      user: req.user._id,
+    };
+
+    product.reviews.push(review);
+
+    product.numReviews = product.reviews.length;
+
+    product.rating =
+      product.reviews.reduce((acc, review) => acc + review.rating, 0) /
+      product.reviews.length;
+
+    await product.save();
+    res.status(201).json({ message: "Review added" });
+  } else {
+    res.status(404);
+    throw new Error("Resource not found");
+  }
+});
+
+export {
+  getProducts,
+  getProductById,
+  createProduct,
+  updateProduct,
+  deleteProduct,
+  createProductReview,
+};
