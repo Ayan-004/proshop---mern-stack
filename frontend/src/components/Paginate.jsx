73ba1bfc6881d41655604186e37f9645f7fd0052
Ayan Shaikh <ayyanjsk@gmail.com>
2025-07-14 18:12:00 +0530

    Create component for pagination

---
+++ frontend/src/components/Paginate.jsx
@@ -0,0 +1,37 @@
+import { Pagination } from "react-bootstrap";
+import { Link } from "react-router-dom";
+
+const Paginate = ({
+  pages,
+  page,
+  isAdmin = false,
+  isUserList = false,
+  isOrderList = false,
+}) => {
+  return (
+    pages > 1 && (
+      <Pagination>
+        {[...Array(pages).keys()].map((x) => (
+          <Pagination.Item
+            key={x + 1}
+            active={x + 1 === page}
+            as={Link}
+            to={
+              !isAdmin
+                ? `/page/${x + 1}`
+                : isUserList
+                ? `/admin/userlist/${x + 1}`
+                : isOrderList
+                ? `/admin/orderlist/${x + 1}`
+                : `/admin/productlist/${x + 1}`
+            }
+          >
+            {x + 1}
+          </Pagination.Item>
+        ))}
+      </Pagination>
+    )
+  );
+};
+
+export default Paginate;
