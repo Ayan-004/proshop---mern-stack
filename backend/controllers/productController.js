aa9b01c5c2d76f80d849aef186799f7badf0dc12
Ayan Shaikh <ayyanjsk@gmail.com>
2025-07-15 16:34:18 +0530

    Add contorller to get top products

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -5,18 +5,23 @@ import Product from "../models/productModel.js";
 // @route GET/api/products
 // @access Public
 const getProducts = asyncHandler(async (req, res) => {
-  const pageSize = 4;
+  const pageSize = 8;
   const page = Number(req.query.pageNumber) || 1;
-  const count = await Product.countDocuments();
 
-  const products = await Product.find({})
+  const keyword = req.query.keyword
+    ? { name: { $regex: req.query.keyword, $options: "i" } }
+    : {};
+
+  const count = await Product.countDocuments({ ...keyword });
+
+  const products = await Product.find({ ...keyword })
     .limit(pageSize)
     .skip(pageSize * (page - 1));
   res.json({ products, page, pages: Math.ceil(count / pageSize) });
 });
 
 // @desc   Fetch a products
-// @route GET/api/products/:id
+// @route  GET/api/products/:id
 // @access Public
 const getProductById = asyncHandler(async (req, res) => {
   const product = await Product.findById(req.params.id);
@@ -28,6 +33,7 @@ const getProductById = asyncHandler(async (req, res) => {
     throw new Error("Resource not found");
   }
 });
+
 
 // @desc   Create a product
 // @route POST/api/products
@@ -54,33 +60,33 @@ const createProduct = asyncHandler(async (req, res) => {
 // @access Private/Admin
 const updateProduct = asyncHandler(async (req, res) => {
   const { name, price, description, image, brand, category, countInStock } =
-    req.body;
+  req.body;
 
   const product = await Product.findById(req.params.id);
 
   if (product) {
     (product.name = name),
-      (product.price = price),
+    (product.price = price),
       (product.description = description),
       (product.image = image),
       (product.brand = brand),
       (product.category = category),
       (product.countInStock = countInStock);
+      
+      const updatedProduct = await product.save();
+      res.json(updatedProduct);
+    } else {
+      res.status(404);
+      throw new Error("Resource not found");
+    }
+  });
 
-    const updatedProduct = await product.save();
-    res.json(updatedProduct);
-  } else {
-    res.status(404);
-    throw new Error("Resource not found");
-  }
-});
-
-// @desc   Delete product
-// @route  DELETE /api/products/:id
-// @access Private/Admin
-const deleteProduct = asyncHandler(async (req, res) => {
-  const product = await Product.findById(req.params.id);
-
+  // @desc   Delete product
+  // @route  DELETE /api/products/:id
+  // @access Private/Admin
+  const deleteProduct = asyncHandler(async (req, res) => {
+    const product = await Product.findById(req.params.id);
+    
   if (product) {
     await Product.deleteOne({ _id: product._id });
     res.status(200).json({ message: "Product deleted" });
@@ -95,9 +101,9 @@ const deleteProduct = asyncHandler(async (req, res) => {
 // @access Private
 const createProductReview = asyncHandler(async (req, res) => {
   const { rating, comment } = req.body;
-
+  
   const product = await Product.findById(req.params.id);
-
+  
   if (product) {
     const alreadyReviewed = product.reviews.find(
       (review) => review.user.toString() === req.user._id.toString()
@@ -107,28 +113,36 @@ const createProductReview = asyncHandler(async (req, res) => {
       res.status(400);
       throw new Error("Product already reviewed");
     }
-
+    
     const review = {
       name: req.user.name,
       rating: Number(rating),
       comment,
       user: req.user._id,
     };
-
+    
     product.reviews.push(review);
-
+    
     product.numReviews = product.reviews.length;
-
+    
     product.rating =
-      product.reviews.reduce((acc, review) => acc + review.rating, 0) /
-      product.reviews.length;
-
+    product.reviews.reduce((acc, review) => acc + review.rating, 0) /
+    product.reviews.length;
+    
     await product.save();
     res.status(201).json({ message: "Review added" });
   } else {
     res.status(404);
     throw new Error("Resource not found");
   }
+});
+
+// @desc   Get top rated products
+// @route  GET/api/products/top
+// @access Public
+const getTopProducts = asyncHandler(async (req, res) => {
+  const products = await Product.find({}).sort({rating: -1}).limit(3);
+  res.status(200).json(products)
 });
 
 export {
@@ -138,4 +152,5 @@ export {
   updateProduct,
   deleteProduct,
   createProductReview,
+  getTopProducts
 };
