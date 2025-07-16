2df8beb14112ea62e7f4bfdc92965266e18a7cf7
Ayan Shaikh <ayyanjsk@gmail.com>
2025-07-16 13:48:33 +0530

    Getting page size from env folder

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -117,7 +117,7 @@ const updateUserProfile = asyncHandler(async (req, res) => {
 // @route GET/api/users
 // @access Private/Admin
 const getUsers = asyncHandler(async (req, res) => {
-    const pageSize = 2;
+    const pageSize = process.env.PAGINATION_LIMIT;
     const page = Number(req.query.pageNumber) || 1;
     const count = await User.countDocuments();
 
